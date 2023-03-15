892a08bc6f810cfe944be0ef6eb78b441bd4affa
LiuYinping <gosickliuyinping@163.com>
2023-03-15 17:15:31 +0800

    second git

--- src/main/java/com/git/example/TestGit.java
+++ src/main/java/com/git/example/TestGit.java
@@ -9,5 +9,8 @@ package com.git.example;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
