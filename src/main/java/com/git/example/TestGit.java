44a8217fdfebee6bcc0760a675e59ca992ff91b9
LiuYinping <gosickliuyinping@163.com>
2023-03-15 17:12:16 +0800

    first git

---
+++ src/main/java/com/git/example/TestGit.java
@@ -0,0 +1,13 @@
+package com.git.example;
+
+/**
+ * @Author: 林断山明
+ * @ClassName: TestGit.java
+ * @From: com.git.example
+ * @Description: TODO
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
